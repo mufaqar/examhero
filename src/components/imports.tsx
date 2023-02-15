9c2db5a70622024790af7ba4103927088492b67d
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2023-02-15 18:57:31 +0500

    updated

--- src/components/imports.tsx
+++ src/components/imports.tsx
@@ -1,6 +1,2 @@
 export {default as Header} from './header';
-<<<<<<< HEAD
 export {default as CompanyBanner} from './companyBanner';
-=======
-export {default as Logo} from './Logo';
->>>>>>> 6873f93d4f0c5479f12a71a14ff4269c226ae9da
