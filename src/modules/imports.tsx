6498073406281376841dbdc13ce1fdb5d4ad3008
syed zunair gillani <gillanix007@gmail.com>
2023-02-16 16:06:33 +0500

    remove conflict

--- src/modules/imports.tsx
+++ src/modules/imports.tsx
@@ -1,6 +1,3 @@
 export {default as Company_Module} from './company-module';
-<<<<<<< HEAD
 export {default as Blog_Module} from './blog-module';
-=======
 export {default as HomeModule} from './home-module';
->>>>>>> 016f40975ed6f0b8bf26f31c31b59c73a3877c96
