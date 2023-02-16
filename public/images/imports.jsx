fb444e90b4235ceb066888002ed1e207a5ac5a21
syed zunair gillani <gillanix007@gmail.com>
2023-02-16 16:03:16 +0500

    remove conflict

--- public/images/imports.jsx
+++ public/images/imports.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export {default as DiamondBD} from './diamond-background.png';
 export {default as Logo} from './logo.png';
 export {default as News_One} from './news-one.png';
@@ -14,8 +13,6 @@ export {default as Post7} from './post7.png';
 export {default as Post8} from './post8.png';
 export {default as Post9} from './post9.png';
 export {default as Post10} from './post10.png';
-=======
-export {default as DiamondBD} from './diamond-background.png'
 export {default as SiteLogo} from './logo.png'
 export {default as HeroImage} from './hero-image.png'
 export {default as P1} from './p1.png'
@@ -27,4 +24,3 @@ export {default as W1} from './w1.png'
 export {default as W2} from './w2.png'
 export {default as W3} from './w3.png'
 export {default as W4} from './w4.png'
->>>>>>> 016f40975ed6f0b8bf26f31c31b59c73a3877c96
--- src/components/imports.tsx
+++ src/components/imports.tsx
@@ -1,13 +1,10 @@
 export {default as Header} from './header';
-<<<<<<< HEAD
 export {default as CompanyBanner} from './companyBanner';
 export {default as CompanyNews} from './companyNews';
 export {default as Blog_Articles} from './blog-articles';
-=======
 export {default as Logo} from './Logo';
 export {default as Main} from './Main';
 export {default as Partners} from './Partners';
 export {default as WhyUs} from './WhyUs';
 export {default as WhatWeDo} from './WhatWeDo';
 export {default as Feedback} from './Feedback';
->>>>>>> 016f40975ed6f0b8bf26f31c31b59c73a3877c96
