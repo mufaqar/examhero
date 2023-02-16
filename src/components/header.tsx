065573e8e3a0d3655bb24b2c97ed660849c668fe
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2023-02-16 16:14:18 +0500

    updated

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,11 @@
 import {Container} from '@/constant/imports'
 import React from 'react'
-<<<<<<< HEAD
 //import {Logo} from './imports'
-=======
 import {Logo} from './imports'
 import Link from 'next/link';
 import { navItems } from '../../public/DataFiles/NavItem'
 import { BiSearch, BiUser } from 'react-icons/bi';
 import {HiOutlineMenuAlt3 } from 'react-icons/hi';
->>>>>>> 016f40975ed6f0b8bf26f31c31b59c73a3877c96
 
 
 interface INavItem {
@@ -21,11 +18,6 @@ function Header() {
   return (
     <header className='py-3 bg-white/50 fixed w-full top-0'>
       <Container>
-<<<<<<< HEAD
-          <div>
-            {/* <Logo /> */}
-
-=======
           <div className='flex items-center justify-between'>
             <Logo/>
             <nav className={`items-center gap-6 lg:gap-10 hidden md:flex`}>
@@ -43,7 +35,6 @@ function Header() {
               {/* hamburger  */}
              <button className='md:hidden'><HiOutlineMenuAlt3/></button>
             </div>
->>>>>>> 016f40975ed6f0b8bf26f31c31b59c73a3877c96
           </div>
           
       </Container>
